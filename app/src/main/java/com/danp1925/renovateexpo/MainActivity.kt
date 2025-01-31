c853bb49572a10e41ba966e5f70642b3e0e7b612
Daniel Peñaloza <DanP1925@gmail.com>
2025-01-31 01:24:02 +0100

    Add coil for patch update

--- app/src/main/java/com/danp1925/renovateexpo/MainActivity.kt
+++ app/src/main/java/com/danp1925/renovateexpo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.danp1925.renovateexpo.ui.theme.RenovateExpoTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +43,11 @@ fun MainScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            AsyncImage(
+                model = "https://media.licdn.com/dms/image/v2/C4E0BAQFXF5Cde9RWig/company-logo_200_200/company-logo_200_200/0/1630588974580?e=1746662400&v=beta&t=jyIP3w_Igf_H2bvPgnTXz4mqz0iCbMG_tx7siLlKp5U",
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth().padding(24.dp)
+            )
             Text(
                 text = "Hola Android Dev Peru",
                 style = TextStyle(fontSize = 32.sp)
