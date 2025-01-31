1752ef5f99423d9e2caa2638f780a474030ab9b4
Daniel Peñaloza <DanP1925@gmail.com>
2025-01-31 01:26:59 +0100

    Downgrade Junit for minor update

--- app/src/test/java/com/danp1925/renovateexpo/ExampleUnitTest.kt
+++ app/src/test/java/com/danp1925/renovateexpo/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.danp1925.renovateexpo
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
